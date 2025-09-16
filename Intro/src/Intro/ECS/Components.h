89695245fc26b29f9a58233a8fbf8712f4db63cc
xiahouzuo <1735327928@qq.com>
2025-09-16 19:02:19 +0800

    11

---
+++ Intro/src/Intro/ECS/Components.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/quaternion.hpp>
+
+
+namespace Intro {
+	struct Transform
+	{
+		glm::vec3 position = glm::vec3(0.0f);
+		glm::quat rotation = glm::quat(1.0f, 0.0f, 0.0f, 0.0f);
+		glm::vec3 scale = glm::vec3(1.0f);
+
+		glm::mat4 GetModelMatrix() const
+		{
+			glm::mat4 scaleMat = glm::scale(glm::mat4(1.0f), scale);
+
+			glm::mat4 rotMat = glm::mat4_cast(rotation);
+
+			glm::mat4 transMat = glm::translate(glm::mat4(1.0f), position);
+
+			return transMat * rotMat * scaleMat;
+		}
+
+		void SetRotationEuler(const glm::vec3& eulerDegrees)
+		{
+			rotation = glm::quat(glm::radians(eulerDegrees));
+		}
+
+		glm::vec3 GetRotationEuler() const
+		{
+			return glm::degrees(glm::eulerAngles(rotation));
+		}
+
+	};
+
+
+	struct TransformComponent
+	{
+		Transform transform;
+	};
+}
